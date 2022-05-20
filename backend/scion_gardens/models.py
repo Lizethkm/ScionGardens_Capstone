8702f1d3f98c78e1da31dab55b30cef925bafa41
Lizeth <mendezlizeth084@gmail.com>
2022-05-20 12:25:33 -0700

    Registerd Location Model in Admin.py File in Location App + Registered Priorities Model in Admin.py File in Priorities App + Registered Reminders App in Admin.py File in Reminders App

--- backend/scion_gardens/models.py
+++ backend/scion_gardens/models.py
@@ -1,13 +1,11 @@
 from django.db import models
 from authentication.models import User
-# from locations.models import Location
 
 # Create your models here.
 
 class PlantCollection(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     plant = models.CharField(max_length=250)
-    # location = models.ForeignKey(Location, blank= True, null= True, on_delete=models.CASCADE) *** Cannot have location before intialization plus location will have plant ID
     sunlight = models.CharField(max_length=250)
     water = models.CharField(max_length=250)
     maintenance = models.CharField(max_length=300)
