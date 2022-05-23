ebb51eb1563c70d09246e3735477f4056842a093
Lizeth <mendezlizeth084@gmail.com>
2022-05-23 15:24:08 -0700

    Displayed Reminders in Home Page + Working on Displaying Locations in Frontend Directory

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -9,36 +9,60 @@ const HomePage = () => {
   // The "token" value is the JWT token that you will send in the header of any request requiring authentication
   //TODO: Add an AddCars Page to add a car for a logged in user's garage
   const [user, token] = useAuth();
-  const [cars, setCars] = useState([]);
+  const [reminders, setReminders] = useState([]);
+  // const [cars, setCars] = useState([]);
   
 
   useEffect(() => {
-    const fetchCars = async () => {
+    const fetchReminders = async () => {
       try {
-        let response = await axios.get("http://127.0.0.1:8000/api/cars/", {
+        let response = await axios.get("http://127.0.0.1:8000/api/reminders/", {
           headers: {
             Authorization: "Bearer " + token,
           },
         });
-        setCars(response.data);
+        setReminders(response.data);
       } catch (error) {
-        console.log(error.response.data);
+        console.log(error.response.data); 
       }
     };
-    fetchCars();
+    fetchReminders();
   }, [token]);
+
+  // useEffect(() => {
+  //   const fetchCars = async () => {
+  //     try {
+  //       let response = await axios.get("http://127.0.0.1:8000/api/cars/", {
+  //         headers: {
+  //           Authorization: "Bearer " + token,
+  //         },
+  //       });
+  //       setCars(response.data);
+  //     } catch (error) {
+  //       console.log(error.response.data);
+  //     }
+  //   };
+  //   fetchCars();
+  // }, [token]);
 
 
 
   return (
     <div className="container">
       <h1>Home Page for {user.username}!</h1>
-      {cars &&
+      {/* {cars &&
         cars.map((car) => (
           <p key={car.id}>
             {car.year} {car.model} {car.make}
           </p>
-        ))}
+        ))} */}
+      <h4>Reminders</h4>
+      {reminders && 
+      reminders.map((el) => (
+        <p key={el.id}>
+          {el.priority} {el.reminder} {el.expired_date}
+        </p>
+      ))}
     </div>
   );
 };
