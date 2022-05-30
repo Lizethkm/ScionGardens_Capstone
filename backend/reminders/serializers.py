c9856df0f10f3efa6f7d44368557ae31b421a041
Lizeth <mendezlizeth084@gmail.com>
2022-05-30 14:39:32 -0700

    Created get_plant_name Function in Reminders Serializers.py File + Added plant_name to Reminders Serializers Fields in Backend

--- backend/reminders/serializers.py
+++ backend/reminders/serializers.py
@@ -6,7 +6,14 @@ from .models import Reminder
 class ReminderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reminder
-        fields =["id", "reminder", "plant", "plant_id", "priority", "created_date", "updated_date", "expired_date"]
-        depth = 1
-    plant_id = serializers.IntegerField(write_only=True)
+        fields =["id", "reminder", "plant", "plant_name", "priority", "created_date", "updated_date", "expired_date"]
+        # depth = 1
+
+    plant_name = serializers.SerializerMethodField("get_plant_name")
+
+    def get_plant_name(self, obj):
+        return obj.plant.plant
+
+
+    # plant_id = serializers.IntegerField(write_only=True)
  
