56ff7412aac95fee794a6fb64f4314143d63255d
Lizeth <mendezlizeth084@gmail.com>
2022-05-30 21:29:30 -0700

    Replaced get_plant_name Function with Plant SlugRelatedField in Reminders App Serializers.py File in Backend

--- backend/reminders/serializers.py
+++ backend/reminders/serializers.py
@@ -1,18 +1,22 @@
 from rest_framework import serializers
 from .models import Reminder
+from scion_gardens.models import PlantCollection
 
 
 
 class ReminderSerializer(serializers.ModelSerializer):
+
+    plant = serializers.SlugRelatedField(slug_field="plant", queryset=PlantCollection.objects.all())
+
     class Meta:
         model = Reminder
-        fields =["id", "reminder", "plant", "plant_name", "priority", "created_date", "updated_date", "expired_date"]
+        fields =["id", "reminder", "plant", "priority", "created_date", "updated_date", "expired_date"]
         # depth = 1
 
-    plant_name = serializers.SerializerMethodField("get_plant_name")
+    # plant_name = serializers.SerializerMethodField("get_plant_name")
 
-    def get_plant_name(self, obj):
-        return obj.plant.plant
+    # def get_plant_name(self, obj):
+    #     return obj.plant.plant
 
 
     # plant_id = serializers.IntegerField(write_only=True)
