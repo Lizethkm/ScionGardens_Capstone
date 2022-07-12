c14c5deeaad7d71e6b63b6432db351b872e663bd
Lizeth <mendezlizeth084@gmail.com>
2022-07-12 12:10:30 -0700

    Styled About Us Page

--- frontend/src/pages/AboutUsPage/AboutUsPage.js
+++ frontend/src/pages/AboutUsPage/AboutUsPage.js
@@ -1,23 +1,40 @@
 import LoginNavBar from "../../components/NavBar/LoginNavBar";
+
 import "./AboutUsPage.css"
 import wateringGarden from "../../assests/wateringGarden.jpg"
+import { useContext } from "react";
+
+import AuthContext from "../../context/AuthContext";
+import Navbar from "../../components/NavBar/NavBar";
 
 
 const AboutUsPage = (props) => {
-
+    const { logoutUser, user } = useContext(AuthContext);
 
     return ( 
+        
         <div className="aboutUsPage">
-            < LoginNavBar />
-            <div >
+            
+                <li>
+                {user ? (
+                    <Navbar />
+                ) : (
+                    < LoginNavBar />
+                )}
+            </li>
+            
+            <div className="aboutUs" style={{ height: "695px"}} >
                 
-                <div>
-                    <img classname="aboutImage" src={wateringGarden} alt="watering plants" />
+                <div classname="aboutImage">
+                    <img  src={wateringGarden} alt="watering plants" style={{height: "600px" }}/>
                 </div>
-                <div className="aboutUs">
+                <div className="aboutUsInfo">
                     <h1>About Scion Gardens</h1>
-                    <h2>Welcome to Scion Gardens</h2>
-                    <h3>Our goal is to ensure your plants get the attention and care they deserve! </h3>                
+                    
+                    <h3>Our goal is to ensure your plants get the attention and care they deserve! </h3> 
+                    <p> With the Scion Gardens App you will be able to add your plants, set a location, add how much sunlight, water, and maintenance they will need. As well as, setting Reminders of different tasks and assign it to a specific
+                        plant. </p>  
+                        <h4> A Happy Plant is a Growing Plant! </h4>             
                 </div>
 
             </div>            
